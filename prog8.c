72bce0fd492a4d6700dca4a7c87d97c47ee627fe
Prajjwal Deep Singh <prajjwal1997@gmail.com>
2018-08-22 23:35:51 +0530

    Add files via upload

---
+++ prog8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	
+	printf("Enter any number of your choice \n");
+	scanf("%d",&a);
+	
+	if(a%2 == 0)
+	{
+		printf("%d is even",a);
+	}
+	else
+	{
+		printf("%d is odd",a);
+	}
+}
